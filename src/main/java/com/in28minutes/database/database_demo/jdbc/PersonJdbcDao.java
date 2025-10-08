e81d5188f88d3e60c071cac67a6cc5cb9c8531f8
Sergey Brovko <serbro7@gmail.com>
2025-10-08 14:02:13 +0200

    Implemented Spring JDBC query method

---
+++ src/main/java/com/in28minutes/database/database_demo/jdbc/PersonJdbcDao.java
@@ -0,0 +1,25 @@
+package com.in28minutes.database.database_demo.jdbc;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import com.in28minutes.database.database_demo.entity.Person;
+
+@Repository
+public class PersonJdbcDao {
+
+	// JdbcTemplate is a resource used in Spring that gives the database connection
+	@Autowired
+	JdbcTemplate jdbcTemplate;
+
+	// This method executes a SQL query using the Person class. We map the SQL query
+	// to the person bean
+	public List<Person> findAll() {
+		return jdbcTemplate.query("select * from person", new BeanPropertyRowMapper(Person.class));
+	}
+
+}
